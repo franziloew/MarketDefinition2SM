4d5ff351ad9b0ca6733e77e0bbed3756a47d28e0
franziloew <franzi@localyzeapp.com>
2022-05-15 14:04:23 +0200

    fix figures

--- docs/func/helper_functions.R
+++
@@ -1,9 +0,0 @@
-substrRight <- function(x, n){
-  substr(x, nchar(x)-n+1, nchar(x))
-}
-
-calculate_week = function(week, year) {
-  date <- lubridate::ymd(paste(year, 1, 1, sep="-"))
-  lubridate::week(date) = week
-  return(date)
-}
